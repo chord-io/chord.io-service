e1cdc7ca16b2dea635f229ccacd93432f9bf172a
Alexis Bekhdadi <alexis.bekhdadi@gmail.com>
2020-02-09 00:20:50 +0100

    fix inheritance and polymorphism when generating swagger document

--- Filters/PolymorphismSchemaFilter.cs
+++ Filters/PolymorphismSchemaFilter.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -10,15 +13,19 @@ namespace Chord.IO.Service.Filters
     public class PolymorphismSchemaFilter : ISchemaFilter
     {
         private readonly Type _type;
-        private readonly List<Type> _derivedTypes;
+        private readonly List<KeyValuePair<Type, Type>> _derivedTypes = new List<KeyValuePair<Type, Type>>();
 
         public PolymorphismSchemaFilter(Type type)
         {
             this._type = type;
-            this._derivedTypes = type.Assembly
+            type.Assembly
                 .GetTypes()
                 .Where(x => type != x && type.IsAssignableFrom(x))
-                .ToList();
+                .ToList()
+                .ForEach(x =>
+                {
+                    this._derivedTypes.Add(new KeyValuePair<Type, Type>(x.BaseType, x));
+                });
         }
 
         public void Apply(OpenApiSchema model, SchemaFilterContext context)
@@ -28,9 +35,48 @@ namespace Chord.IO.Service.Filters
                 return;
             }
 
-            model.OneOf = this._derivedTypes
-                .Select(x => context.SchemaGenerator.GenerateSchema(x, context.SchemaRepository))
-                .ToList();
+            model.Extensions = new Dictionary<string, IOpenApiExtension>
+            {
+                {"x-abstract", new OpenApiBoolean(true)}
+            };
+
+            this._derivedTypes
+                .ForEach(x => context.SchemaGenerator.GenerateSchema(x.Value, context.SchemaRepository));
+
+            this._derivedTypes
+                .GroupBy(x => x.Key)
+                .ToList()
+                .ForEach(x =>
+                {
+                    var baseSchema = x.Key == this._type ? model : context.SchemaRepository.Schemas[x.Key.Name];
+                    foreach (var type in x)
+                    {
+                        var schema = context.SchemaRepository.Schemas[type.Value.Name];
+                        var properties = schema.Properties.Keys
+                            .Except(baseSchema.Properties.Keys)
+                            .ToDictionary(z => z, z => schema.Properties[z]);
+
+                        context.SchemaRepository.Schemas[type.Value.Name] = new OpenApiSchema
+                        {
+                            Required = schema.Required
+                                .Except(baseSchema.Required)
+                                .ToHashSet(),
+                            Properties = properties,
+                            AllOf = new List<OpenApiSchema>
+                            {
+                                new OpenApiSchema
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Id = x.Key.Name,
+                                        Type = ReferenceType.Schema
+                                    }
+                                }
+                            }
+
+                        };
+                    }
+                });
         }
     }
 }
