bc402eda132c42c87680d53f9ce98170483d470e
Alexis Bekhdadi <alexis.bekhdadi@gmail.com>
2020-02-16 14:29:55 +0100

    add track ability to deserialyze derieved classes

--- Models/Hierarchy/Tracks/Track.cs
+++ Models/Hierarchy/Tracks/Track.cs
@@ -3,12 +3,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using Chord.IO.Service.Services;
+using JsonSubTypes;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
 
 namespace Chord.IO.Service.Models.Hierarchy.Tracks
 {
+    [JsonConverter(typeof(JsonSubtypes))]
+    [JsonSubtypes.KnownSubTypeWithProperty(typeof(MidiTrack), nameof(MidiTrack.Channel))]
+    [JsonSubtypes.KnownSubTypeWithProperty(typeof(DrumTrack), nameof(DrumTrack.DrumMapId))]
     public abstract class Track : IValidatableObject
     {
         [MinLength(5, ErrorMessage = "Value {0} require a minimum length of {1} character")]
